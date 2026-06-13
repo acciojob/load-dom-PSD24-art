40b753b9c09a77d2f7af7af10ebc56dd0a7e866c
Prathamesh S. Dahake <pratham.dahakepsd@gmail.com>
2026-06-13 17:10:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//your JS code here. If required
+
+let el =document.createElement("p")
+el.innerText = "DOM Load success"
+
+window.addEventListener("DOMContentLoaded", ()=>{
+	document.body.appendChild(el);
+})
+ 
